14c3b1c48b8886a0e73599ce6df3cafd26a80994
limaantonio <limaantoniocarlos20@gmail.com>
2020-03-18 19:44:26 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/ac/curso/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.ac.curso.repositories;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ac.curso.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String>{
+	
+}
--- src/main/java/com/ac/curso/resources/UserResource.java
+++ src/main/java/com/ac/curso/resources/UserResource.java
@@ -1,28 +1,26 @@
 package com.ac.curso.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ac.curso.domain.User;
+import com.ac.curso.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 
+	@Autowired
+	private UserService service;
+	
 	@RequestMapping(method = RequestMethod.GET)
 	public ResponseEntity<List<User>> findAll(){
-		User maria = new User("1", "Maria Silva", "Maria@gmail.com");
-		User a = new User("2", "Alex", "a@gmail.com");
-		
-		List<User> users = new ArrayList<>();
-		users.addAll(Arrays.asList(maria, a));
-		
+		List<User> users = service.findAll();
 		return ResponseEntity.ok().body(users);
 	}
 }
